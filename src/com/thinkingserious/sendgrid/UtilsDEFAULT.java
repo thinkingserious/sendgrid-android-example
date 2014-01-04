f2459b60a440e79c07b8d76e6886b47629373609
Elmer Thomas <elmer.thomas@sendgrid.com>
2014-01-03 17:33:30 -0800

    Added default sample config.

---
+++ src/com/thinkingserious/sendgrid/UtilsDEFAULT.java
@@ -0,0 +1,17 @@
+package com.thinkingserious.sendgrid;
+
+// Change the name of this file to Utils.java and rename the class to Utils.
+// Drop in your own SendGrid credentials
+
+public class UtilsDEFAULT {
+	public static final String username = "Sendgrid-Name" ;
+    public static final String password = "SendGrid-Password" ;
+
+    public String getUsername() {
+    	return username;
+    }
+    
+    public String getPassword() {
+    	return password;
+    }
+}
